7c325512db7c55e02b944966188a48a1d3b8d47d
Bogdan Sklyarov <n1ghtm6r9@gmail.com>
2025-06-11 18:02:12 +0300

    feat(): update tg logs

--- src/TelegramNotification/TelegramNotificationModule.ts
+++ src/TelegramNotification/TelegramNotificationModule.ts
@@ -1,3 +1,4 @@
+import { encode } from 'he';
 import { Module, Global } from '@nestjs/common';
 import { configKey, IConfig } from '@nmxjs/config';
 import { getFullMessageFromError } from '@nmxjs/utils';
@@ -16,14 +17,26 @@ import { telegramNotifierKey } from './constants';
         }
         const telegramClients = config.notification.telegrams.map(telegramConfig => new TelegramBot(telegramConfig.token));
 
-        const send = ({ message }) =>
+        const send = ({ message }, options?) =>
           Promise.all(
-            telegramClients.map((client, i) => config.notification.telegrams[i].chatIds.map(chatId => client.sendMessage(chatId, message))),
+            telegramClients.map((client, i) => config.notification.telegrams[i].chatIds.map(chatId => client.sendMessage(chatId, message, options))),
           ).then();
 
         return {
           send,
-          sendError: error => send({ message: getFullMessageFromError({ error }) }),
+          sendError: error => {
+            const errorMessage = `
+            <b>Error</b>
+            
+            <blockquote expandable>${encode(getFullMessageFromError({ error }))}</blockquote>
+            `;
+            return send(
+              {
+                message: errorMessage,
+              },
+              { parse_mode: 'HTML' },
+            );
+          },
         };
       },
       inject: [configKey],
